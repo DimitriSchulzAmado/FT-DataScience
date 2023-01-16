21985938fa910d6cab06a293d44557a7884333ce
“Dimitri” <dimitri.s.amado@gmail.com>
2023-01-16 19:18:42 -0300

    mudanca

---
+++ arrasta/application2.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from pathlib import Path
+from tkinter import ttk
+from tkinter.ttk import Frame
+import pandas as pd
+from tkdnd import DND_FILES
+from tkinterdnd2 import TkinterDnD
+
+'''A extensão tkdnd fornece uma interface para mecanismos nativos de arrastar e soltar
+específicos da plataforma . '''
+
